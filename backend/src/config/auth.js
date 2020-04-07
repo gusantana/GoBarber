092a8ba997b903ec8a1fb454095e28fa85f6bc0d
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2020-04-07 10:59:36 -0400

    backend

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret: "5335877b287ede0e8aaaad2f7dc2ac63",
+    expiresIn: "7d",
+};
---
+++ backend/src/config/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+    development: {
+        dialect: "sqlite",
+        storage: "./src/database/database.sqlite3",
+    },
+    test: {
+        dialect: "sqlite",
+        storage: ":memory",
+    },
+    production: {
+        dialect: "sqlite",
+        storage: "./src/database/database.sqlite3",
+    },
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+};
