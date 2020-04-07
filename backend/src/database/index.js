211b4153cd43e1c3e895b35e8653483e02a4946c
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2020-04-07 15:37:57 -0400

    inserindo avatar do usuário

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,20 +1,26 @@
-import Sequelize from 'sequelize';
+import Sequelize from "sequelize";
 
-import User from '../app/models/User';
+import User from "../app/models/User";
+import File from "../app/models/File";
 
-import databaseConfig from '../config/database';
+import databaseConfig from "../config/database";
 
-const models = [User];
+const models = [User, File];
 
 class Database {
     constructor() {
         this.init();
     }
 
-    init(){
+    init() {
         this.connection = new Sequelize(databaseConfig.development);
 
-        models.map(model => model.init(this.connection));
+        models
+            .map((model) => model.init(this.connection))
+            .map(
+                (model) =>
+                    model.associate && model.associate(this.connection.models)
+            );
     }
 }
 
