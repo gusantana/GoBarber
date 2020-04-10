14f657784ccb32c20f9a88eae7153d06de5abd66
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2020-04-10 12:15:34 -0400

    Envio de email ao cancelar um agendamento

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+    host: "smtp.mailtrap.io",
+    port: "2525",
+    secure: false,
+    auth: {
+        user: "f192f3df2b8b9a",
+        pass: "af36e77ae7d3f1",
+    },
+    default: {
+        from: "Equipe Lotus <noreply@lotus.com.br>",
+    },
+};
