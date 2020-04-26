22abae0a41cc53bd03942de09384f322374a4890
Gustavo Santana Coelho <gustavo_13roo@hotmail.com>
2020-04-25 21:11:48 -0400

    autenticação com redux

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -36,6 +36,14 @@ module.exports = {
 		camelcase: 'off',
 		'no-console': ['error', { allow: ['tron'] }],
 		'react-hooks/rules-of-hooks': 'error',
-		'react-hooks/exhaustive-deps': 'warn'
+		'react-hooks/exhaustive-deps': 'warn',
+	},
+	settings: {
+		'import/resolver': {
+			'babel-plugin-root-import': {
+				rootPathPrefix: '~',
+				rootPathSuffix: 'src',
+			},
+		},
 	},
 };
--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-// import { Container } from './styles';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
+
+import { signInRequest } from '~/store/modules/auth/actions';
+
+import logo from '~/assets/logo.png';
+
+const schema = Yup.object().shape({
+	email: Yup.string()
+		.email('Insira um e-mail válido')
+		.required('O e-mail é obrigatório'),
+	password: Yup.string().required('A senha é obrigatória'),
+});
 
 export default function SignIn() {
-  return (
-	<h1>Login</h1>
-  );
+	const dispatch = useDispatch();
+	const loading = useSelector((state) => state.auth.loading);
+
+	function handleSubmit({ email, password }) {
+		dispatch(signInRequest(email, password));
+	}
+
+	return (
+		<>
+			<img src={logo} alt="" />
+
+			<Form schema={schema} onSubmit={handleSubmit}>
+				<Input
+					name="email"
+					type="email"
+					placeholder="Seu e-mail"
+					id=""
+				/>
+				<Input
+					name="password"
+					type="password"
+					placeholder="Sua senha"
+					id=""
+				/>
+
+				<button type="submit">{loading ? 'Carregando...' : 'Acessar'}</button>
+				<Link to="/register">Criar conta gratuita</Link>
+			</Form>
+		</>
+	);
 }
